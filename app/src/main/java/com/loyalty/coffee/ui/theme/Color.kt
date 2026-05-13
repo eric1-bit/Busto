0563398f1532cb8071a0e8434b8ca58529ad576d
eric1-bit <ericprikhodko@gmail.com>
2026-05-13 17:33:18 +0300

    Create Color.kt

---
+++ app/src/main/java/com/loyalty/coffee/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.loyalty.coffee.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Coffee900 = Color(0xFF3D2B1F)
+val Coffee800 = Color(0xFF5C4033)
+val Coffee700 = Color(0xFF6F4E37)
+val Coffee600 = Color(0xFF8B6F5C)
+val Coffee500 = Color(0xFFA0826D)
+val Coffee400 = Color(0xFFB8956A)
+val Coffee300 = Color(0xFFD4A373)
+val Coffee200 = Color(0xFFE8D5B7)
+val Coffee100 = Color(0xFFF5E8C7)
+val Coffee50 = Color(0xFFFFF8F0)
